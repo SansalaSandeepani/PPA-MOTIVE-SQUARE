e3c8397cf92f008fbb52f402cfb2c8b43ba9a82c
Yashini2003 <yashodhaaponsu@gmail.com>
2025-08-18 18:32:28 +0530

    Update staff route.js

--- backend/roots/staff route.js
+++ backend/roots/staff route.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const router = express.Router();
 
+const {  saveStaff,
+    getAllStaff,
+    updateStaff,  
+    deleteStaff,
+    getStaffById } = require('../Controller/Staff-Controller');
+
+// router.post('/', savestaff);
+router.post('/', async (req, res, next) => {
+  try {
+    await saveStaff(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get('/get', getAllStaff);
+router.put('/:id', updateStaff);
+router.delete('/:id', deleteStaff);
+router.get('/:id', getStaffById);
+
+module.exports = router;
