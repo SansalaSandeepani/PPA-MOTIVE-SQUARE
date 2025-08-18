226fc0f75effbe464f6ea7519b01a8c6ce0e4ae5
SansalaSandeepani <sansalasandeepani222@gmail.com>
2025-08-18 16:07:44 +0530

    Update task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,1 +1,37 @@
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const TaskSchema = new Schema({
+
+    time: {
+       type: String,   // format: "HH:mm" (24h) or "hh:mm AM/PM"
+       required: true,
+    },   
+    date: {
+        type: String,   // format: "YYYY-MM-DD"
+        required: true,
+    },
+    Task_Id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    Task: {
+        type: String,
+        required: true,
+    },
+    answer: {
+        type: String,
+        required: true,
+    },
+    discription: {
+        type: String,
+        required: true
+    }
+})
+
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
